6ffd4ff13eef39429fb0e91a930b51d01caa0fc0
jhon.cordoba <jhon.cordoba@smartquick.com.co>
2021-11-25 00:08:05 -0500

    some changes

---
+++ complex_time.py
@@ -0,0 +1,33 @@
+"""This is an example about how to see the process time in a code"""
+
+import time
+
+def factorial(n):
+    respuesta = 1
+
+    while n > 1:
+        respuesta *= n
+        n -= 1
+
+    return respuesta
+
+
+def factorial_r(n):
+    if n == 1:
+        return 1
+
+    return n * factorial(n - 1)
+
+
+if __name__ == '__main__':
+    n = 200000
+
+    comienzo = time.time()
+    factorial(n)
+    final = time.time()
+    print(final - comienzo)
+
+    comienzo = time.time()
+    factorial_r(n)
+    final = time.time()
+    print(final - comienzo)
